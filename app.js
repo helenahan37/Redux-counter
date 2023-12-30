5cf5d3fe1f460baf43f448e91f43d923b3a022f7
Helena.Han <hlyhelena37@gmail.com>
2023-12-30 19:26:54 +1100

    create reducer

---
+++ app.js
@@ -0,0 +1,67 @@
+const { createReducer, configureStore, createAction } = require('@reduxjs/toolkit');
+const { nanoid } = require('redux');
+
+//Initial state
+const initialState = {
+	counter: 0,
+};
+
+//Action creator
+const increment = createAction('INCREMENT');
+const decrement = createAction('DECREMENT');
+const resetcounter = createAction('RESET');
+
+const incrementBy = createAction('INCREMENT_BY', (amount, user) => {
+	return {
+		payload: {
+			amount,
+		},
+	};
+});
+
+//Reducer
+const counterSlice = createReducer(initialState, (builder) => {
+	//increment
+	builder.addCase(increment, (state) => {
+		state.counter += 1;
+	});
+	//decrement
+	builder.addCase(decrement, (state) => {
+		state.counter -= 1;
+	});
+	//reset
+	builder.addCase(resetcounter, (state) => {
+		state.counter = 0;
+	});
+	//incrementBy
+	builder.addCase(incrementBy, (state, action) => {
+		state.counter += action.payload.amount;
+	});
+});
+
+//map object notation
+// const counterSlice2 = createAction(initialState, {
+// 	[increment]: (state) => {
+// 		state.counter += 1;
+// 	},
+// 	[decrement]: (state) => {
+// 		state.counter -= 1;
+// 	},
+// 	[resetcounter]: (state) => {
+// 		state.counter = 0;
+// 	},
+// 	[incrementBy]: (state, action) => {
+// 		state.counter += action.payload.amount;
+// 	},
+// });
+
+//store
+const store = configureStore({
+	reducer: counterSlice,
+});
+
+//dispatch
+store.dispatch(increment());
+store.dispatch(incrementBy(100));
+
+console.log(store.getState());
