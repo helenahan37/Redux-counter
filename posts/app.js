2253d4af7b1936c328bd8eaa680e3ea1233916bd
Helena.Han <hlyhelena37@gmail.com>
2024-01-02 20:40:44 +1100

    create post slice

--- posts/app.js
+++ posts/app.js
@@ -1,4 +1,5 @@
-const { createAsyncThunk } = require('@reduxjs/toolkit');
+const { createAsyncThunk, createSlice, configureStore } = require('@reduxjs/toolkit');
+
 const axios = require('axios');
 
 //inital state
@@ -10,6 +11,40 @@ const initialState = {
 	error: null,
 };
 //create async thunk
-createAsyncThunk('posts/fetchPosts', async () => {});
+const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+	const res = await axios.get(API);
+	return res.data;
+});
 
 //slice
+const postSlice = createSlice({
+	name: 'posts',
+	initialState,
+	extraReducers: (builder) => {
+		//handle pending, fulfilled, rejected lifecycle actions
+		builder.addCase(fetchPosts.pending, (state, action) => {
+			state.loading = true;
+		});
+		builder.addCase(fetchPosts.fulfilled, (state, action) => {
+			state.posts = action.payload;
+			state.loading = false;
+		});
+		builder.addCase(fetchPosts.rejected, (state, action) => {
+			state.posts = [];
+			state.loading = false;
+			state.error = action.error;
+		});
+	},
+});
+
+//generate reducer
+const postsReducer = postSlice.reducer;
+
+//store
+const store = configureStore({
+	reducer: postsReducer,
+});
+
+//dispatch
+store.subscribe(() => console.log(store.getState()));
+store.dispatch(fetchPosts());
